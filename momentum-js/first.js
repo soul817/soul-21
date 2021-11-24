834585da9a9fe40d55d00adee9437b7ced9c4a04
soul817 <90231949+soul817@users.noreply.github.com>
2021-11-24 23:45:08 +0900

    js-1

---
+++ momentum-js/first.js
@@ -0,0 +1,14 @@
+console.log(1025) ;
+console.log("Miss you");
+
+const a =5;
+const b=2;
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+
+const c="i";
+const d="like";
+const e="you";
+console.log(c+d+e);
+console.log("love" +d);
